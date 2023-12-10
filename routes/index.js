d48805addd9664747ca5c2955fd9c53b1efd06b7
João Victor Guidorizi <127569199+guidorizi@users.noreply.github.com>
2023-12-10 19:47:45 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const categoriesRoutes = require('./categoriesRoutes');
 const usersRoutes = require('./usersRoutes');
+const carsRoutes = require('./carsRoutes'); 
 
 const router = express.Router();
 
 router.use('/categories', categoriesRoutes);
 router.use('/users', usersRoutes);
+router.use('/cars', carsRoutes);
 
 module.exports = router;
