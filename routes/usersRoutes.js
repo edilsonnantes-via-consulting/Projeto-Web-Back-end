757dbbd69c8df840b8c48ec8d94f308e4b9287a3
João Victor Guidorizi <127569199+guidorizi@users.noreply.github.com>
2023-12-10 18:12:26 -0300

    Create usersRoutes.js

---
+++ routes/usersRoutes.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+const { v4: uuidv4 } = require('uuid');
+const sequelize = require('./db');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUIDV4,
+    defaultValue: () => uuidv4(),
+    primaryKey: true,
+    allowNull: false,
+    unique: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  driver_license: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  admin: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
+
+module.exports = User;
